f570c164896f64e10e73f31cb0855a905a1ca075
Jonathan Bernal <165844592+JKarim92@users.noreply.github.com>
2025-08-13 19:39:52 -0500

    Inicializado proyecto de nómina con estructura de autenticar y registro

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Importar libreria para conectar a SQL
+using System.Data;
+using System.Data.SqlClient;
+
+//Importar otras funcionalidades
+using System.Security.Cryptography;
+using System.Configuration;
+
+namespace aplicacionNomina.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public ActionResult Autenticar()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace aplicacionNomina.Models
+{
+    public class Empleado 
+    {
+        public string cedula {  get; set; }
+
+        public string birth_date { get; set; }
+
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public char genero { get; set; }
+
+        public string hire_date { get; set; }
+
+        public string correo { get; set; }
+
+        public string usuario { get; set; }
+
+        public string clave { get; set; }
+
+        public string id { get; set; }
+
+    }
+}
