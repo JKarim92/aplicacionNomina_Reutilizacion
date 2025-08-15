c963a52100c4b2db489318abd1fcdac0953c1bc9
Jonathan Bernal <165844592+JKarim92@users.noreply.github.com>
2025-08-15 09:23:31 -0500

    Creacion codigo enlace base de datos

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -11,24 +11,49 @@ using System.Data.SqlClient;
 //Importar otras funcionalidades
 using System.Security.Cryptography;
 using System.Configuration;
+using aplicacionNomina.Models;
+using System.Text;
+using System.Web.UI.WebControls;
 
 namespace aplicacionNomina.Controllers
 {
     public class AccesoController : Controller
     {
+        //Metodos Get - Donde obtenemos información
+        [HttpGet]
         public ActionResult Autenticar()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
+        [HttpGet]
         public ActionResult Registro()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
+        //Metodos Post - enviamos información
+        [HttpPost]
+        public ActionResult Autenticar(Empleado oEmpleado)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cnn"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("select * from tbl_employees");
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e);
+                return View();
+            }
+        }
     }
 }
